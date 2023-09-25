cf83728f73a5b4ae3892ecda6b4f3f780a07ef6b
Oliver Richardson <123179922+Olliekse@users.noreply.github.com>
2023-09-26 01:17:09 +0300

    created basic functions

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(add(5, 2));
+
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+console.log(subtract(5, 2));
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+console.log(multiply(5, 2));
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
+
+console.log(divide(4, 2));

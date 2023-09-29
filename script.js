4f063fd2a5e903eb3c5cbf079a4b3ef68f6dcd71
Oliver Richardson <123179922+Olliekse@users.noreply.github.com>
2023-09-29 13:04:52 +0300

    finished basic functionality

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-let num1 = 5;
-let num2 = 5;
-let op = "+";
-
 const btnsContainer = document.querySelector(".ui__btns-container");
-const btnsNum = document.querySelector(".btns-num-container");
-const btnsFunc = document.querySelector(".btns-func-container");
+const screen = document.querySelector(".ui__screen-digits");
 
 const add = (num1, num2) => {
   return num1 + num2;
@@ -22,7 +17,31 @@ const divide = (num1, num2) => {
   return num1 / num2;
 };
 
+const numsFuncs = [
+  1,
+  2,
+  3,
+  "+",
+  4,
+  5,
+  6,
+  "-",
+  7,
+  8,
+  9,
+  "*",
+  " ",
+  0,
+  " ",
+  "/",
+  "C",
+  ".",
+  "%",
+  "=",
+];
+
 const operate = (num1, op, num2) => {
+  console.log(num1, op, num2);
   if (op === "+") {
     return add(num1, num2);
   } else if (op === "-") {
@@ -34,23 +53,49 @@ const operate = (num1, op, num2) => {
   }
 };
 
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, " ", 0, " ", "C", ".", "%"];
-
-const funcs = ["+", "-", "*", "/", "="];
-
 const createBtns = () => {
-  for (i = 0; i < nums.length; i++) {
-    const btn = document.createElement("div");
-    btn.classList.add("btn", "btn_num");
-    btn.textContent = nums[i];
-    btnsNum.append(btn);
+  let num1 = 0;
+  let num2 = 0;
+  let inputSequence = 0;
+  let operator = "";
+  let screenSequence;
+
+  screen.textContent = 0;
+
+  for (i = 0; i < numsFuncs.length; i++) {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.textContent = numsFuncs[i];
+    btn.type = "button";
+    btnsContainer.append(btn);
+
+    btn.addEventListener("click", (e) => {
+      const input = e.target.textContent;
+      if (inputSequence === 0 && !isNaN(input)) {
+        num1 = parseInt(input);
+        screenSequence = num1;
+        screen.textContent = screenSequence;
+        inputSequence++;
+      } else if (
+        inputSequence === 1 &&
+        (input === "+" || input === "-" || input === "*" || input === "/")
+      ) {
+        operator = input;
+        screenSequence += operator;
+        screen.textContent = screenSequence;
+        inputSequence++;
+      } else if (inputSequence === 2 && !isNaN(input)) {
+        num2 = parseInt(input);
+        screenSequence = `${num1}${operator}${num2}`;
+        screen.textContent = screenSequence;
+        inputSequence++;
+      } else if (inputSequence === 3 && input === "=") {
+        screen.textContent = operate(num1, operator, num2);
+        console.log(operate(num1, operator, num2));
+        inputSequence = 0;
+      }
+    });
   }
 };
-for (i = 0; i < funcs.length; i++) {
-  const btn = document.createElement("div");
-  btn.classList.add("btn", "btn_func");
-  btn.textContent = funcs[i];
-  btnsFunc.append(btn);
-}
 
 createBtns();
